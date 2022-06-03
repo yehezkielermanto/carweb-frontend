2798bdcf35e7cbbc158890cdf8d8051581bbdf32
yehezkielermanto <yehezkiel.ermanto28@gmail.com>
2022-06-03 20:40:06 +0700

    update frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,25 +65,25 @@ const NavbarComponent = () => {
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link
-                    href="http://localhost:3000/#ourService"
+                    href="https://challenge-7-tim5-frontend.herokuapp.com/#ourService"
                     className="fw-bold my-2 mx-2"
                   >
                     Our Service
                   </Nav.Link>
                   <Nav.Link
-                    href="http://localhost:3000/#whyUs"
+                    href="https://challenge-7-tim5-frontend.herokuapp.com/#whyUs"
                     className="fw-bold my-2 mx-2"
                   >
                     Why Us
                   </Nav.Link>
                   <Nav.Link
-                    href="http://localhost:3000/#testimonial"
+                    href="https://challenge-7-tim5-frontend.herokuapp.com/#testimonial"
                     className="fw-bold my-2 mx-2"
                   >
                     Testimonial
                   </Nav.Link>
                   <Nav.Link
-                    href="http://localhost:3000/#faq"
+                    href="https://challenge-7-tim5-frontend.herokuapp.com/#faq"
                     className="fw-bold my-2 mx-2"
                   >
                     FAQ
